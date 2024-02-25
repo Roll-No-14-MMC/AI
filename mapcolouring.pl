394fdeb1deb5b76fbd1aaee6e003a603124ae28a
Roll-No-14-MMC <156989419+Roll-No-14-MMC@users.noreply.github.com>
2024-02-25 10:41:07 +0545

    map colouring

---
+++ mapcolouring.pl
@@ -0,0 +1,17 @@
+colouring(A,B,C,D,E,F):-
+    different(A,B),
+    different(A,C),
+    different(A,D),
+    different(A,F),
+    different(B,C),
+    different(B,E),
+    different(C,D),
+    different(C,E),
+    different(D,E),
+    different(E,F).
+    different(red,green).
+    different(green,yellow).
+    different(red,yellow).
+    different(green,red).
+    different(yellow,green).
+    different(yellow,red).
